c3f982f19257bff5da57095c4a8c23c7b76da266
Pmnikam <Pmnikam@users.noreply.github.com>
2023-02-21 21:46:16 +0530

    Add files via upload

---
+++ ECommerceWithSpringMVC/src/main/java/com/e1/exception/GlobalExceptionHandeler.java
@@ -0,0 +1,14 @@
+package com.e1.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandeler {
+	@ExceptionHandler(value=Exception.class)
+	public String handleAnyException()
+
+	{
+		return "Error";
+	}
+}
